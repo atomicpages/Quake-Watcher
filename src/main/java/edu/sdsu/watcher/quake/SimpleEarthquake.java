494d1575ae4fde1a70b3295e308bd47be770a14f
Dennis Thompson <djtthompson@gmail.com>
2016-04-24 16:21:29 -0700

    Caching implementation done

--- src/main/java/edu/sdsu/watcher/quake/SimpleEarthquake.java
+++ src/main/java/edu/sdsu/watcher/quake/SimpleEarthquake.java
@@ -2,9 +2,7 @@ package edu.sdsu.watcher.quake;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -15,8 +13,8 @@ import com.google.common.base.Preconditions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParser;
+import edu.sdsu.watcher.quake.cache.Cache;
 import edu.sdsu.watcher.quake.factories.DecodeJson;
-import edu.sdsu.watcher.quake.io.FileIOHelper;
 import edu.sdsu.watcher.quake.net.JsonReader;
 import edu.sdsu.watcher.quake.net.Reader;
 import edu.sdsu.watcher.quake.structures.QuakeStruct;
@@ -24,15 +22,15 @@ import edu.sdsu.watcher.quake.structures.QuakeStruct;
 /**
  * Handles the details of getting the data from the remote or local resource,
  * filtering the data, and returning the user with usable data.
+ *
  * @author Dennis Thompson
  */
 public class SimpleEarthquake {
 
 	private static final Reader jsonReader = JsonReader.getInstance();
-	private static QuakeStruct struct = null;
-
 	private static final String USGS_URL = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/";
 
+	private static QuakeStruct struct = null;
 	private static HashSet<String> time, threshold;
 
 	static {
@@ -52,9 +50,9 @@ public class SimpleEarthquake {
 	}
 
 	private boolean online;
-	private boolean cacheJson;
-	private File cache;
-	private Path cachePath;
+	private boolean cacheJson, enablePrettyCache;
+	private File cacheFile;
+	private Cache cache, prettyCache;
 
 	/**
 	 * Reads live data from the USGS website.
@@ -64,7 +62,7 @@ public class SimpleEarthquake {
 	}
 
 	/**
-	 * <p>Construct a new Object with caching ability. If you don't need the ability to cache then usage of this
+	 * <p>Construct a new Object with caching ability. If you don't need the ability to cacheFile then usage of this
 	 * constructor is discouraged &ndash; use {@link SimpleEarthquake#getEarthquakes()} for live data or use</p>
 	 * <p>Note: This constructor will first gather the live data once {@link SimpleEarthquake#getEarthquakes(String, String)}
 	 * is called and then save the {@code json} data in the location specified. Sample usage:</p>
@@ -72,68 +70,52 @@ public class SimpleEarthquake {
 	 *     SimpleEarthquake test = new SimpleEarthquake(true, "/home/foo/Desktop");
 	 *     List&lt;Earthquake&gt; quakes = test.getEarthquakes("all", "hour");
 	 * </pre>
-	 * {@link SimpleEarthquake#SimpleEarthquake(File)} to read from an existing cache.
-	 * @param cache Set true to cache the downloaded {@code json} file.
-	 * @param location The path of the file without the file (this will be created automatically).
+	 * {@link SimpleEarthquake#SimpleEarthquake(File)} to read from an existing cacheFile.
+	 *
+	 * @param cacheFile Set true to cache the downloaded {@code geojson} file.
+	 * @param location  The path of the file without the file (this will be created automatically).
 	 */
-	public SimpleEarthquake(final boolean cache, final String location) {
+	public SimpleEarthquake(final boolean cacheFile, final String location) {
 		this();
-		this.cacheJson = cache;
+		this.cacheJson = cacheFile;
 
 		if(this.cacheJson) {
-			if(location.matches(".*[^\\.\\w+]$")) {
-				Path path = Paths.get(location);
-				if(!Files.exists(path)) {
-					FileIOHelper.createDirectory(path);
-				}
-				this.cachePath = path;
-			} else { // prepare string
-				// saves last little bit of the path
-				final String temp = location.substring(location.lastIndexOf(System.getProperty("file.separator")) + 1);
-				if(!temp.matches("\\w+\\.\\w+")) { // does not have a file structure
-					final Path path = Paths.get(com.google.common.io.Files.simplifyPath(location));
-					FileIOHelper.createDirectory(path);
-					this.cachePath = path;
-				} else {
-					final String[] segments = location.split(System.getProperty("file.separator"));
-					final Path path = Paths.get(segments[segments.length - 2]);
-					FileIOHelper.createDirectory(path);
-					this.cachePath = path;
-				}
-			}
+			this.cache = new Cache(null, location);
 		}
 	}
 
 	/**
 	 * Reads a local file and uses cached data.
+	 *
 	 * @param file the cached {@code json} file to read from.
 	 */
 	public SimpleEarthquake(final File file) {
 		Preconditions.checkNotNull(file);
 		Preconditions.checkArgument(file.exists() && file.canRead());
 		this.online = false;
-		this.cache = file;
+		this.cacheFile = file;
 	}
 
 	/**
 	 * <p>Gets a list of earthquakes found within the given threshold and time.</p>
 	 * <p>Acceptable units for threshold are:</p>
 	 * <ul>
-	 *     <li>significant</li>
-	 *     <li>all</li>
-	 *     <li>4.5</li>
-	 *     <li>2.5</li>
-	 *     <li>1.0</li>
+	 * <li>significant</li>
+	 * <li>all</li>
+	 * <li>4.5</li>
+	 * <li>2.5</li>
+	 * <li>1.0</li>
 	 * </ul>
 	 * <p>Acceptable units for time are:</p>
 	 * <ul>
-	 *     <li>month</li>
-	 *     <li>week</li>
-	 *     <li>day</li>
-	 *     <li>hour</li>
+	 * <li>month</li>
+	 * <li>week</li>
+	 * <li>day</li>
+	 * <li>hour</li>
 	 * </ul>
+	 *
 	 * @param threshold the threshold to filter by.
-	 * @param time the time to filer by.
+	 * @param time      the time to filer by.
 	 * @return a list of earthquakes or an empty list if none are found.
 	 */
 	public List<Earthquake> getEarthquakes(String threshold, String time) {
@@ -157,19 +139,19 @@ public class SimpleEarthquake {
 				final String url = String.format("%s%s_%s.geojson", USGS_URL, threshold, time);
 				final String json = jsonReader.get(url);
 				if(this.cacheJson) {
-					final String cacheFilePath = this.cachePath.toString() + System.getProperty("file.separator");
-					final String cacheFileName = String.format("%s_%s.geojson", threshold, time);
-					final String cacheFileNamePretty = String.format("pretty_%s_%s.geojson", threshold, time);
-
-					FileIOHelper.write(cacheFilePath + cacheFileName, json);
+					this.cache.setName(String.format("%s_%s", threshold, time));
+					this.cache.append(json);
 
 					// TODO: allow user to configure this
 //					FileIOHelper.write(cacheFilePath + cacheFileNamePretty, prettyPrintJson(json));
 				}
 				struct = DecodeJson.parse(json, QuakeStruct.class);
 			} else {
-				struct = DecodeJson.parse(this.cache, QuakeStruct.class);
+				struct = DecodeJson.parse(this.cacheFile, QuakeStruct.class);
 			}
+		} catch(NoSuchFileException e) {
+			System.err.println("Unable to write to cache file " + this.cache.getName());
+			e.printStackTrace();
 		} catch(IOException e) {
 			System.err.println("A parse error occurred! Check the stack trace for more details.");
 			e.printStackTrace();
@@ -181,6 +163,7 @@ public class SimpleEarthquake {
 
 	/**
 	 * <p>Gets a list of <strong>all</strong> earthquakes found within a week time frame.</p>
+	 *
 	 * @return a list of earthquakes or an empty list if none are found.
 	 * @see SimpleEarthquake#getEarthquakes(String, String)
 	 */
@@ -203,8 +186,9 @@ public class SimpleEarthquake {
 	/**
 	 * Handles the creation of the custom error messages when the time or threshold intervals
 	 * are incorrect.
+	 *
 	 * @param prepend Prepends the buffer with a custom message.
-	 * @param set The set to combine into a comma-delimited list.
+	 * @param set     The set to combine into a comma-delimited list.
 	 * @return A comma-delimited list with a custom prepended message.
 	 */
 	private static String buildErrors(final String prepend, final Set<String> set) {
@@ -215,6 +199,7 @@ public class SimpleEarthquake {
 
 	/**
 	 * Filters the list using the default filter.
+	 *
 	 * @param list The list to filter.
 	 * @return A list of filtered Earthquake objects.
 	 */
@@ -232,6 +217,7 @@ public class SimpleEarthquake {
 
 	/**
 	 * Defines the filter rules for the Earthquake object.
+	 *
 	 * @param features The features list to filter.
 	 * @return an Earthquake object containing all of the items needed from the features list.
 	 */
@@ -240,11 +226,11 @@ public class SimpleEarthquake {
 		final double lon = features.getGeometry().getCoordinates().get(0);
 		final double lat = features.getGeometry().getCoordinates().get(1);
 		final double depth = features.getGeometry().getCoordinates().get(2);
-
 		final double magnitude = features.getProperties().getMag();
-		final String description = features.getProperties().getTitle();
+
 		final long time = features.getProperties().getTime();
 
+		final String description = features.getProperties().getTitle();
 		final String id = features.getId();
 
 		return new Earthquake(new Coordinate(lon, lat, depth), magnitude, description, id, time);
@@ -260,6 +246,10 @@ public class SimpleEarthquake {
 
 		final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		return gson.toJson(new JsonParser().parse(uglyJson));
+	}
+
+	public static void main(String... args) {
+		new SimpleEarthquake(true, "/Users/dennis/Desktop/cache_data/").getEarthquakes("all", "hour");
 	}
 
 }
--- src/main/java/edu/sdsu/watcher/quake/cache/Cache.java
+++ src/main/java/edu/sdsu/watcher/quake/cache/Cache.java
@@ -1,5 +1,6 @@
 package edu.sdsu.watcher.quake.cache;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,15 +12,16 @@ import edu.sdsu.watcher.quake.io.FileIOHelper;
  * <p>A simple Cache class helper that aides in creating and updating existing cache files.
  * This class does not:</p>
  * <ul>
- *     <li>Set expiration times on caches</li>
- *     <li>Invalidate expired caches</li>
- *     <li>Store caches outside of the local file system</li>
- *     <li>Store caches in a dictionary, database, map, or otherwise a general Object</li>
+ *      <li>Set expiration times on caches</li>
+ *      <li>Invalidate expired caches automatically</li>
+ *      <li>Store caches outside of the local file system</li>
+ *      <li>Store caches in a dictionary, database, map, or otherwise a general Object</li>
  * </ul>
  * <p>This class does:</p>
  * <ul>
- *     <li>Create files at a specified location</li>
- *     <li>Adds a timestamp to the name of the file (can be configured)</li>
+ *      <li>Create files at a specified location</li>
+ *      <li>Adds a timestamp to the name of the file (can be configured)</li>
+ *      <li>Provide uses with the ability to run CRUD operations on existing caches</li>
  * </ul>
  */
 public class Cache {
@@ -27,51 +29,60 @@ public class Cache {
 	private static final long TIME = System.currentTimeMillis();
 	private static final String DEFAULT_FILE_EXT = ".json";
 
-	private String name, path;
+	private String name, location, extension;
 	private boolean appendTimestamp;
+	private Path path;
 
 	/**
-	 * @param cacheFileName The name of the cache file.
-	 * @param cachePath The path to store the cache file.
+	 * @param fileName The name of the cache file.
+	 * @param location The location to store the cache file.
 	 */
-	public Cache(String cacheFileName, String cachePath) {
-		this(cacheFileName, cachePath, DEFAULT_FILE_EXT);
+	public Cache(String fileName, String location) {
+		this(fileName, location, DEFAULT_FILE_EXT);
 	}
 
 	/**
-	 * @param cacheFileName The name of the cache file.
-	 * @param cachePath The path to store the cache file.
+	 * @param fileName        The name of the cache file.
+	 * @param location        The location to store the cache file.
 	 * @param appendTimestamp Set true to append the timestamp to the cache file name.
 	 */
-	public Cache(String cacheFileName, String cachePath, boolean appendTimestamp) {
-		this(cacheFileName, cachePath, DEFAULT_FILE_EXT, appendTimestamp);
+	public Cache(String fileName, String location, boolean appendTimestamp) {
+		this(fileName, location, DEFAULT_FILE_EXT, appendTimestamp);
 	}
 
 	/**
-	 * @param cacheFileName The name of the cache file.
-	 * @param cachePath The path to store the cache file.
+	 * @param fileName  The name of the cache file.
+	 * @param location  The location to store the cache file.
 	 * @param extension The extension to give the cache file.
 	 */
-	public Cache(String cacheFileName, String cachePath, String extension) {
-		this(cacheFileName, cachePath, extension, true);
+	public Cache(String fileName, String location, String extension) {
+		this(fileName, location, extension, true);
 	}
 
 	/**
-	 * @param cacheFileName The name of the cache file.
-	 * @param cachePath The path to store the cache file.
-	 * @param extension The extension to give the cache file.
+	 * @param fileName        The name of the cache file.
+	 * @param location        The location to store the cache file.
+	 * @param extension       The extension to give the cache file.
 	 * @param appendTimestamp Set true to append the timestamp to the cache file name.
 	 */
-	public Cache(String cacheFileName, String cachePath, String extension, boolean appendTimestamp) {
-		Preconditions.checkNotNull(cacheFileName, cachePath, extension);
-		Preconditions.checkArgument(!cacheFileName.isEmpty(), !cachePath.isEmpty());
-		Preconditions.checkArgument(cachePath.matches(".*[^\\.\\w+]$")); // make no attempt to fix the path
+	public Cache(String fileName, String location, String extension, boolean appendTimestamp) {
+		Preconditions.checkNotNull(location, extension);
+		Preconditions.checkArgument(!location.isEmpty());
+		Preconditions.checkArgument(location.matches(".*[^\\.\\w+]$"), "Malformed cache location. Missing trailing slash."); // make no attempt to fix the location
+		Preconditions.checkArgument(extension.matches("\\.\\w+"));
 
+		this.extension = extension;
 		this.appendTimestamp = appendTimestamp;
-		this.name = cacheFileName;
-		if(this.appendTimestamp) this.name += TIME;
-		this.name += normalizeExtension(extension);
-		this.path = cachePath;
+		this.name = fileName;
+
+		if(this.name != null && !this.name.isEmpty()) {
+			if(this.appendTimestamp) {
+				this.name += TIME;
+			}
+			this.name += normalizeExtension(extension);
+		}
+
+		this.location = location;
 		this.initialize();
 	}
 
@@ -90,10 +101,71 @@ public class Cache {
 	}
 
 	/**
-	 * @return The path to the cache file.
+	 * <p>Overrides the name of the cache <em>only when the name supplied to the constructor
+	 * is {@code null} or an empty string.</em> This is a basic defense mechanism
+	 * that prevents the name of the file being changed during runtime.</p>
+	 *
+	 * @param name The name of the cache file.
 	 */
-	public String getPath() {
-		return this.path;
+	public void setName(final String name) {
+		Preconditions.checkNotNull(name);
+		Preconditions.checkArgument(!name.isEmpty());
+
+		if(this.name == null || this.name.isEmpty()) {
+			this.name = name;
+			if(this.appendTimestamp) this.name += TIME;
+			this.name += this.extension;
+			this.path = Paths.get(this.location, this.name);
+		}
+	}
+
+	/**
+	 * Appends data to the cache file.
+	 * @param content The message to append to the cache file.
+	 * @throws IOException Thrown upon error when writing to the file.
+	 */
+	public void append(String content) throws IOException {
+		Preconditions.checkNotNull(this.path);
+		CacheWriter.append(this.path, content);
+	}
+
+	/**
+	 * Truncates an existing cache file or creates one if it does not exist.
+	 * @param content The message to append to the cache file.
+	 * @throws IOException Thrown upon error when writing to the file.
+	 */
+	public void create(String content) throws IOException {
+		Preconditions.checkNotNull(this.path);
+		CacheWriter.create(this.path, content);
+	}
+
+	/**
+	 * @return True if the operation was successful.
+	 */
+	public boolean delete() {
+		Preconditions.checkNotNull(this.path);
+		return CacheWriter.delete(this.path);
+	}
+
+	/**
+	 * @return The location to the cache file.
+	 */
+	public String getLocation() {
+		return this.location;
+	}
+
+	private void initialize() {
+		Path path = null;
+
+		if(this.name != null && !this.name.isEmpty()) {
+			this.path = path = Paths.get(this.location, this.name);
+		} else {
+			path = Paths.get(this.location);
+		}
+
+		if(!Files.exists(path)) {
+			FileIOHelper.createDirectory(path);
+		}
 	}
 
 	private static String normalizeExtension(String ext) {
@@ -105,14 +177,6 @@ public class Cache {
 		}
 
 		return ext;
-	}
-
-	private void initialize() {
-		final Path path = Paths.get((this.path));
-
-		if(!Files.exists(path)) {
-			FileIOHelper.createDirectory(path);
-		}
 	}
 
 }
