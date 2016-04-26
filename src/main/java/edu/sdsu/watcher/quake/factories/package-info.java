e81cf4292b4314a10c3eaafe7d610da02c0e5652
Dennis Thompson <djtthompson@gmail.com>
2016-04-26 16:27:52 -0700

    init commit

--- src/main/java/edu/sdsu/watcher/quake/factories/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Contains all of the factories (i.e. classes that handle the details of object creation)
- * for various tasks.
- * @author Dennis Thompson
- */
-package edu.sdsu.watcher.quake.factories;
